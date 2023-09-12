27f8ecbef4584d497958710b72af7b234e8931fe
Andres Triana <73327110+AndresTrianaD@users.noreply.github.com>
2023-09-12 15:08:56 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app"
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAkflQd_TL9GJFhPnLKabtEVwYcMf50CUM",
-    authDomain: "prueba-udec.firebaseapp.com",
-    projectId: "prueba-udec",
-    storageBucket: "prueba-udec.appspot.com",
-    messagingSenderId: "598318990956",
-    appId: "1:598318990956:web:a54279d2fcb8d061d83a49",
-    measurementId: "G-TCSVEV3D0T"
+    apiKey: "ssssssssssssssssss",
+    authDomain: "ssssssssssssssssssss",
+    projectId: "sssssssssssssssssss",
+    storageBucket: "sssssssssssssssssssssssss",
+    messagingSenderId: "ssssssssssssssssssssss",
+    appId: "sssssssssssssssssssssssssssssssss",
+    measurementId: "ssssssssssssssss"
   };
 
   export const firebaseApp =firebase. initializeApp(firebaseConfig)
